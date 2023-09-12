e0b0e0023276ff5a3c3bf4e0ae8482dc9b18c898
jeremy <projeremylegendre@gmail.com>
2023-09-12 10:06:34 +0200

    first commit

---
+++ myFirstPooPlugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package myFirstPooPlugin
+ */
+/*
+Plugin name: myFirstPooPlugin
+Description : premier plugin wordpress en poo 
+Version :1
+Plugin URI: https://github.com/ggdelamul/firstPooWordPressPlugin.git
+Author : Jérémy Legendre 
+Author URI : https://jeremy-leg-dev.netlify.app
+License : GPL v2 or later 
+Text domain : myFirstPooPlugin
+*/ 
